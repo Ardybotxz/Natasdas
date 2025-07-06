ca6444df390fe81b9177956da98d2b6b2edd949c
Ardyyy <86863270+ArdyBotzz@users.noreply.github.com>
2025-07-06 16:50:49 +0800

    Update NatHub.lua

--- NatHub.lua
+++ NatHub.lua
@@ -50,9 +50,12 @@ if script_id then
             Duration = 5
         }
     )
-    local auth = _function.load("https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/keysystem.lua")
-    local auth_status = auth(script_id)
-    repeat task.wait() until auth_status.validated
+    if not premium then
+        local auth = _function.load("https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/keysystem.lua")
+        local auth_status = auth(script_id)
+        repeat task.wait() until auth_status.validated
+    end
+    script_key = script_key
     if premium then premium = true; is_premium = true end
     _function.load("https://api.luarmor.net/files/v4/loaders/" .. script_id .. ".lua")
 end
